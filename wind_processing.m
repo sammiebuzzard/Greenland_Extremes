b66b110cb34c0eb1c72fb551b8f4ad7b0e4117dc
Sam Cornish <sam.b.cornish@gmail.com>
2018-09-19 16:39:48 +0100

    made matlabwind processing

---
+++ wind_processing.m
@@ -0,0 +1,30 @@
+% tried to do this in Python but still too novice
+% need to load in Kap_Morris_2018 data
+% interested in Wind heading and speed columns
+% add requisite paths
+% load requisite file
+
+v = WND_SPD.*cos(WND_HD*pi/180); % calculating the meridional component of wind
+v(v>100)=NaN; % eliminating fill values
+
+%1 Feb - 28 Feb: 744:1413
+%1 July - 31 Aug: 4290:5729
+
+v_feb = v(744:1413);
+v_Jul_Aug = v(4290:5729);
+x_feb = [744/24,744/24];
+y_feb = [-40,40];
+x_Jul = [4290/24, 4290/24];
+y_Jul = [-40,40];
+
+
+t_feb = (1:length(v_feb))./24;
+t_Jul_Aug = (1:length(v_Jul_Aug))./24;
+t_all = (1:length(v))./24;
+
+figure; plot(t_feb, v_feb); xlabel('Days from 1 Feb 2018 00:00'); ylabel('meridional component of wind, units unknown')
+figure; plot(t_Jul_Aug,v_Jul_Aug); xlabel('Days from 1 Jul 2018 00:00'); ylabel('meridional component of wind, units unknown')
+
+figure; plot(t_all,v); xlabel('Days from 1 Jan 2018'); ylabel('meridional component of wind, units unkn.')
+hold on
+plot(x_feb,y_feb); plot(x_Jul,y_Jul);
